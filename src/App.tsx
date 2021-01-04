be96ce30f5d87e1ade69f1272870a3f9ece7c8da
darguelles <leinad87@gmail.com>
2021-01-04 16:18:47 +0100

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import { Button, Alert, Card, Navbar, Container, Image } from 'react-bootstrap';
 import DropZone from "./dropzone/DropZone";
+import GitHub_Logo_White from './images/GitHub_Logo_White.png';
+
 function App() {
 
 
@@ -27,28 +29,39 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar bg="dark" sticky="top">
+        <Container>
+          <a href="https://github.com/leinad87/IBD6"><Image src={GitHub_Logo_White} width="100px"/></a>
+        </Container>
+      </Navbar >
       <header className="App-header">
-
         <div className="content">
-          <p className="info">
-            Crea en InteractiveBrokers una nueva consulta flex con las siguientes secciones y campos:
-          <ul>Posiciones Abiertas
-          <ul><li>Symbol 	</li>
+          <Alert variant='warning'>⚠️ Este software puede tener errores, uselo bajo su responsabilidad.</Alert>
+
+          <DropZone />
+
+          <iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=30&amp;p=13&amp;l=ez&amp;f=ifr&amp;linkID=7765fbe0e479f8f3ad9e7791a36a6604&amp;t=leinad87-21&amp;tracking_id=leinad87-21"
+            width="468" height="60" scrolling="no"
+            style={{ border: "none", margin: 10 }}></iframe>
+
+          <Card className="mt-5">
+            <Card.Body><p className="info">
+              Crea en InteractiveBrokers una nueva consulta flex con las sección <b>Posiciones Abiertas</b> y campos:
+              <ul className="info">
+                <li>Symbol</li>
                 <li>Description</li>
                 <li>ISIN</li>
                 <li>Quantity</li>
                 <li>Multiplier</li>
                 <li>Position Value</li>
                 <li>Mark Price</li>
-                <li>Currency</li></ul></ul>
+                <li>Currency</li>
+              </ul>
+              Ejecuta la consulta con el perido <b>Mes pasado</b> y format <b>CSV</b>. Guarda el fichero y arrastreloo para generar el informe D6.
+              </p>
+            </Card.Body>
+          </Card>
 
-                AVISO: Este software puede tener errores, uselo bajo su responsabilidad.
-          </p>
-          <DropZone />
-          
-          <iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=30&amp;p=13&amp;l=ez&amp;f=ifr&amp;linkID=7765fbe0e479f8f3ad9e7791a36a6604&amp;t=leinad87-21&amp;tracking_id=leinad87-21"
-            width="468" height="60" scrolling="no"
-            style={{ border: "none", margin: 10 }}></iframe>
         </div>
       </header>
     </div>

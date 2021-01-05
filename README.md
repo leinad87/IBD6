82644170bcb79107b0f7b958d248c26845fa588f
darguelles <leinad87@gmail.com>
2021-01-05 13:09:27 +0100

    Added summary

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # IBD6
-
+[![Netlify Status](https://api.netlify.com/api/v1/badges/cecc4a0c-3dcb-4fa3-abdd-77dc6a965016/deploy-status)](https://app.netlify.com/sites/ibd6/deploys)
 Esta aplicación carga un extracto de una consulta FlexQuery de Interactive Brokers y genera un fichero .aforixm con el contenido para el informe D6 de España.
 
 Puedes usar el software aqui: https://ibd6.netlify.app/
--- src/dropzone/DropZone.tsx
+++ src/dropzone/DropZone.tsx
@@ -1,84 +1,116 @@
 import React from 'react';
+import { Button, Alert, Card, Navbar, Container, Image } from 'react-bootstrap';
 import { D6 } from '../aforix/D6';
 import { Position } from '../aforix/Position';
 
 import './DropZone.css';
 
-const downloadTxtFile = (content: string) => {
-    const element = document.createElement("a");
-    const file = new Blob([content], { type: 'application/aforixm' });
-    element.href = URL.createObjectURL(file);
-    element.download = "myfile.aforixm";
-    document.body.appendChild(element); // Required for this to work in FireFox
-    element.click();
-}
+export default class DropZone extends React.Component<{}, { text: string }> {
 
-// https://blog.logrocket.com/create-a-drag-and-drop-component-with-react-dropzone/
-const dragOver = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-}
+    constructor(props: any) {
+        super(props);
+        this.state = { text: '' }
+    }
 
-const dragEnter = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-}
+    downloadTxtFile = (content: string) => {
+        const element = document.createElement("a");
+        const file = new Blob([content], { type: 'application/aforixm' });
+        element.href = URL.createObjectURL(file);
+        element.download = "myfile.aforixm";
+        document.body.appendChild(element); // Required for this to work in FireFox
+        element.click();
+    }
 
-const dragLeave = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-}
+    // https://blog.logrocket.com/create-a-drag-and-drop-component-with-react-dropzone/
+    dragOver = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+    }
 
-const fileDrop = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    const files = e.dataTransfer.files;
-    var file = files[0]
-    var data1 = file.text();
-    data1.then((res) => {
-        var aforix = new D6()
+    dragEnter = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+    }
 
-        var headers: { [key: string]: number } = {};
+    dragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+    }
 
-        var skip = true;
-        res.split("\n").forEach((line) => {
-            if (line.includes("ISIN")) {
-                skip = false
-                line.split(",").forEach((val, idx) => {
-                    headers[val.slice(1, -1)] = idx;
-                })
-            } else if (!skip && line.length > 0) {
-                var data = line.split(",").map((x) => x.slice(1, -1))
-                var position = new Position(
-                    data[headers['ISIN']],
-                    data[headers['Description']],
-                    parseInt(data[headers['Quantity']]),
-                    parseFloat(data[headers['PositionValue']]),
-                    data[headers['CurrencyPrimary']]
-                )
-                aforix.add_position(position)
+    fileDrop = (e: React.DragEvent<HTMLDivElement>) => {
+        e.preventDefault();
+        const files = e.dataTransfer.files;
+        var file = files[0]
+        var data1 = file.text();
+
+
+        data1.then((res) => {
+            var total_ammount: { [key: string]: number } = {}
+            var aforix = new D6()
+
+            var headers: { [key: string]: number } = {};
+
+            var skip = true;
+            res.split("\n").forEach((line) => {
+                if (line.includes("ISIN")) {
+                    skip = false
+                    line.split(",").forEach((val, idx) => {
+                        headers[val.slice(1, -1)] = idx;
+                    })
+                } else if (!skip && line.length > 0) {
+                    var data = line.split(",").map((x) => x.slice(1, -1));
+
+                    var count = parseInt(data[headers['Quantity']]);
+                    var value = parseFloat(data[headers['PositionValue']]);
+                    var currency = data[headers['CurrencyPrimary']];
+
+                    var position = new Position(
+                        data[headers['ISIN']],
+                        data[headers['Description']],
+                        count,
+                        value,
+                        currency
+                    )
+                    aforix.add_position(position)
+
+                    // Sum the value of the position to summary
+                    var tmp = total_ammount[currency] || 0;
+                    total_ammount[currency] = tmp + value
+                    console.log(total_ammount)
+
+                }
+
+            })
+            var content = aforix.build();
+            this.downloadTxtFile(content);
+
+            var total_ammount_str = '';
+            for (var key in total_ammount) {
+                total_ammount_str = total_ammount_str + ` ${key}: ${total_ammount[key].toLocaleString('es-ES', { minimumFractionDigits: 2 })}`;
             }
 
+            this.setState({ text: `Se han encontrado ${aforix.positions.length} posiciones abiertas con un valor total de ${total_ammount_str}` });
+
         })
-        var content = aforix.build();
-        console.log(content)
-        downloadTxtFile(content)
-    })
-}
+    }
 
 
-const DropZone = () => {
-    return (
-        <div className="container">
-            <div className="drop-container"
+    render() {
+        return (
+            <div className="container">
+                <div className="drop-container"
 
-                onDragOver={dragOver}
-                onDragEnter={dragEnter}
-                onDragLeave={dragLeave}
-                onDrop={fileDrop}
+                    onDragOver={this.dragOver}
+                    onDragEnter={this.dragEnter}
+                    onDragLeave={this.dragLeave}
+                    onDrop={this.fileDrop}
                 >
-                <div className="drop-message">
-                    <div className="upload-icon"></div>
-                    <p className="title">Arrastra aquí tu fichero csv con la consulta Flex</p>
+                    <div className="drop-message">
+                        <div className="upload-icon"></div>
+                        <p className="title">Arrastra aquí tu fichero csv con la consulta Flex</p>
+                    </div>
                 </div>
+                {this.state.text.length > 0 ?
+                    (<Alert variant="dark" className="mt-3">{this.state.text}</Alert>) : null
+                }
             </div>
-        </div>
-    )
+        )
+    }
 }
-export default DropZone;

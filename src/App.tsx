c288ab4a339030e007ecce1b93cf021ead404a5b
darguelles <leinad87@gmail.com>
2021-01-05 14:06:25 +0100

    updated amazon message

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
     <div className="App">
       <Navbar bg="dark" sticky="top">
         <Container>
-          <a href="https://github.com/leinad87/IBD6"><Image src={GitHub_Logo_White} width="100px"/></a>
+          <a href="https://github.com/leinad87/IBD6"><Image src={GitHub_Logo_White} width="100px" /></a>
         </Container>
       </Navbar >
       <header className="App-header">
@@ -40,9 +40,11 @@ function App() {
 
           <DropZone />
 
+          <p>Si quieres colaborar puedes hacerlo en <a target="_blank" href="https://github.com/leinad87/IBD6">https://github.com/leinad87/IBD6</a> o comprar en <a target="_blank" href="https://www.amazon.es?&amp;_encoding=UTF8&amp;tag=leinad87-21&amp;linkCode=ur2&amp;linkId=76dc4efc60d1bc7a8017891fefbb86a4&amp;camp=3638&amp;creative=24630">Amazon.es</a><img src="//ir-es.amazon-adsystem.com/e/ir?t=leinad87-21&amp;l=ur2&amp;o=30" width="1" height="1" /> a través del enlace :)</p>
           <iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=30&amp;p=13&amp;l=ez&amp;f=ifr&amp;linkID=7765fbe0e479f8f3ad9e7791a36a6604&amp;t=leinad87-21&amp;tracking_id=leinad87-21"
             width="468" height="60" scrolling="no"
             style={{ border: "none", margin: 10 }}></iframe>
+
 
           <Card className="mt-5">
             <Card.Body><p className="info">

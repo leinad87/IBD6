87eca780c4a7be125be5b1353fa3ebd5a075bb34
darguelles <leinad87@gmail.com>
2021-01-15 18:19:30 +0100

    fix degiro format

--- src/parsers/degiro.tsx
+++ src/parsers/degiro.tsx
@@ -11,15 +11,15 @@ export default class DegiroParser {
     static parse(file: string, default_pais: string, default_emisor: number, default_valor: number, holders: number): Position[] {
         var headers: { [key: string]: number };
         var positions: Position[] = [];
-        var re = /""(\d+),(\d+)""/g
-        
+        var re = /"(\d+),(\d+)"/g
+
         file.split("\n").forEach((line) => {
             if (line.includes("ISIN")) {
                 headers = DegiroParser.getHeaders(line)
             } else if (line.length > 0) {
                 // remove quotes
-                var data = line.slice(1, -1).replaceAll(re ,"$1.$2").split(",")
-                
+                var data = line.replaceAll(re, "$1.$2").split(",")
+                console.log(data)
                 if (!data[headers[NAME]].includes("CASH")) {
                     var { value, currency } = DegiroParser.getCurrencyValue(data[headers[VALUE]]);
                     positions.push(new Position(
@@ -52,7 +52,7 @@ export default class DegiroParser {
     static getCurrencyValue(price: string) {
         var value: number;
         var currency: string;
-        
+
         var data = price.split(" ");
         value = parseFloat(data[1]);
         currency = data[0];
